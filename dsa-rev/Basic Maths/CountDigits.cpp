dda5b9d53e5f1f42b5f0324a5e06719526eb96cd
DhruvGahoi <dhruvgahoi11111@gmail.com>
2024-11-20 11:03:12 +0530

    Count number of digits

---
+++ dsa-rev/Basic Maths/CountDigits.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int count(int n) {
+    int cnt = 0;
+    while(n > 0) {
+        cnt++;
+        int lastDigit = n % 10;
+        n = n / 10; 
+    }
+    return cnt;
+}
+
+int main() {
+    int n;
+    cout << "Enter the number" << endl;
+    cin >> n;
+    cout << "The number of digits are : " << count(n) << endl;
+
+}
