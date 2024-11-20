289b48f44416c1e783948334fa33d363604da7e2
DhruvGahoi <dhruvgahoi11111@gmail.com>
2024-11-20 11:05:52 +0530

    Another log based approach

--- dsa-rev/Basic Maths/CountDigits.cpp
+++ dsa-rev/Basic Maths/CountDigits.cpp
@@ -1,13 +1,17 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int count(int n) {
-    int cnt = 0;
-    while(n > 0) {
-        cnt++;
-        int lastDigit = n % 10;
-        n = n / 10; 
-    }
+    // int cnt = 0;
+    // while(n > 0) {
+    //     cnt++;
+    //     int lastDigit = n % 10;
+    //     n = n / 10; 
+    // }
+    // return cnt;
+
+    //----------------------------
+    int cnt = (int)(log10(n) + 1);
     return cnt;
 }
 
