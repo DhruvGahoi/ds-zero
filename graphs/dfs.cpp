8bc546b53c21bdbc4eb864e6e03f5c3abbabd54e
DhruvGahoi <dhruvgahoi11111@gmail.com>
2024-11-26 13:54:34 +0530

    Depth First Search

---
+++ graphs/dfs.cpp
@@ -0,0 +1,25 @@
+// --------------------------DFS----------------------------
+
+class Solution {
+    private : 
+        void dfs(int node, vector<vector<int>>& adj, int vis[], vector<int> &ls){
+            vis[node] = 1;
+            ls.push_back(node);
+
+            for(auto it : adj[node]){
+                if(!vis[it]){
+                    dfs(it, adj, vis, ls);
+                }
+            }
+        }
+
+    public : 
+        vector<int> dfsOfGraph(vector<vector<int>>& adj){
+            int n = adj.size();
+            int vis[n] = {0};
+            int start = 0;
+            vector<int> ls;
+            dfs(start, adj, vis, ls);
+            return ls;
+        }
+}
