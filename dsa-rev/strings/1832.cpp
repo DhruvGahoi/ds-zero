762bdb2a36a08443eb9389f4e50a2a561db98efa
DhruvGahoi <dhruvgahoi11111@gmail.com>
2024-12-12 13:58:42 +0530

    strings 01

---
+++ dsa-rev/strings/1832.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        unordered_set<char> uniqueChars;
+        for (char ch : sentence) {
+            uniqueChars.insert(ch);
+        }
+        return uniqueChars.size() == 26;
+    }
+};
+
