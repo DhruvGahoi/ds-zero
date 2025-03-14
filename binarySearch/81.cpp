7efd1863e3dc8983ad2851f0d461cf270aa35da2
DhruvGahoi <dhruvgahoi11111@gmail.com>
2025-03-14 10:53:45 +0530

    Str: Rotated Sorted Array - II

--- binarySearch/81.cpp
+++ binarySearch/81.cpp
@@ -47,3 +47,41 @@ public:
         return BS(pivot, n - 1, nums, target);
     }
 };
+
+
+// Striversssssssssssss
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0, high = n - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target){
+                return true;
+            }
+
+            if(nums[mid] == nums[low] && nums[mid] == nums[high]){
+                low = low + 1;
+                high = high - 1;
+                continue;
+            }
+
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && target <= nums[mid]){
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                 if(nums[mid] <= target && target <= nums[high]){
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+};
