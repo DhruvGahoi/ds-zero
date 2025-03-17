e5c7107894a7e28a4bbf85203f4cf44b51bf805f
DhruvGahoi <dhruvgahoi11111@gmail.com>
2025-03-17 09:47:13 +0530

    Lower and Upper bound

---
+++ binarySearch/lowerUpperBound.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+
+class Solution {
+public: 
+    int lowerBound(vector<int>& arr, int x){
+        int n = arr.size();
+        int low = 0, high = n - 1;
+        int ans = n;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(arr[mid] >= x){
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+    int upperBound(vector<int>& arr, int x){
+        int n = arr.size();
+        int low = 0, high = n - 1;
+        int ans = n;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(arr[mid] > x){
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+};
